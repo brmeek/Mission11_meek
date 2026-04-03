c9ffa77a538db3a85877485885cb0dae7bd5eaf3
Brit Meek <bmeek1@byu.edu>
2026-04-02 18:01:40 -0600

    getting ready to deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://bookstore-backend-hrceaafxeyd9akfy.westus2-01.azurewebsites.net',
         changeOrigin: true,
       },
     },
